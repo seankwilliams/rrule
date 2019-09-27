9b99658d23e45ca762df0354694e13deeef70165
Sean Williams <sean@prismtechstudios.com>
2019-09-27 11:12:15 -0600

    Fix performance issue when iterating over large date ranges with between()

--- src/datewithzone.ts
+++ src/datewithzone.ts
@@ -1,13 +1,17 @@
 import dateutil from './dateutil'
-import { DateTime } from 'luxon'
+import { DateTime, IANAZone } from 'luxon'
 
 export class DateWithZone {
   public date: Date
   public tzid?: string | null
+  public static ianaZones: {[index: string]: IANAZone}
 
   constructor (date: Date, tzid?: string | null) {
     this.date = date
     this.tzid = tzid
+    if (typeof DateWithZone.ianaZones === 'undefined') {
+      DateWithZone.ianaZones = {}
+    }
   }
 
   private get isUTC () {
@@ -36,7 +40,15 @@ export class DateWithZone {
       const datetime = DateTime
         .fromJSDate(this.date)
 
-      const rezoned = datetime.setZone(this.tzid!, { keepLocalTime: true })
+      // get the IANAZone for Luxon
+      // this is pregenerated and stored in an object, because otherwise, Luxon has to recreate it each time,
+      // which is a very time consuming process when calculating wide date ranges
+      if (!(this.tzid!.toLowerCase() in DateWithZone.ianaZones)) {
+        DateWithZone.ianaZones[this.tzid!.toLowerCase()] = new IANAZone(this.tzid!.toLowerCase())
+      }
+      let ianaZone = DateWithZone.ianaZones[this.tzid!.toLowerCase()]
+
+      const rezoned = datetime.setZone(ianaZone, { keepLocalTime: true })
 
       return rezoned.toJSDate()
     } catch (e) {
